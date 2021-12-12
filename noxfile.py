c0412cb3d7abad287641dc04a31f1dca48c3b243
Andrzej Klajnert <github@aklajnert.pl>
2021-12-12 09:37:55 +0100

    Switch to GitHub actions, drop support for Python 3.4 and 3.5 (#9)

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.5", "3.6", "3.7", "3.8", "3.9", "pypy3"])
+@nox.session(python=["3.6", "3.7", "3.8", "3.9", "3.10"])
 def tests(session):
     session.install(".[test]")
     session.run(
